bd03e08a7af1d835e64b67abe1929419148761a3
david <davyd_d4v1d@hotmail.com>
2019-09-30 02:43:52 -0500

    Se puede vender producto

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,14 @@
       <v-card-title style="background-color:#5CD5C6">
         Productos
         <div class="flex-grow-1"></div>
-        <v-text-field style="margin-bottom:15px;" v-model="search" append-icon="search" label="Buscar" single-line hide-details></v-text-field>
+        <v-text-field
+          style="margin-bottom:15px;"
+          v-model="search"
+          append-icon="search"
+          label="Buscar"
+          single-line
+          hide-details
+        ></v-text-field>
         <div class="flex-grow-1"></div>
         <v-dialog v-model="dialog" max-width="500px">
           <template v-slot:activator="{ on }">
@@ -42,7 +49,31 @@
         hide-default-footer
         class="elevation-1"
         @page-count="pageCount = $event"
-      ></v-data-table>
+      >
+        <template v-slot:item.action="{ item }">
+          <v-dialog v-model="dialog2" max-width="250px">
+            <template v-slot:activator="{ on }">
+              <v-icon small class="mr-2" title="Vender" v-on="on">mdi-tag</v-icon>
+            </template>
+            <v-card>
+              <v-card-title>
+                <span class="headline">Cantidad</span>
+              </v-card-title>
+
+              <v-card-text>
+                <v-text-field v-model="cantidad" type="number" label="Cantidas"></v-text-field>
+              </v-card-text>
+
+              <v-card-actions>
+                <div class="flex-grow-1"></div>
+                <v-btn color="blue darken-1" text @click="close">Cancelar</v-btn>
+                <v-btn color="blue darken-1" text @click="vender(item)">Vender</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+          <v-icon small title="Borrar" @click="deleteItem(item)">delete</v-icon>
+        </template>
+      </v-data-table>
       <div class="text-center pt-2">
         <v-pagination v-model="page" :length="pageCount"></v-pagination>
       </div>
@@ -56,6 +87,8 @@ export default {
   data() {
     return {
       dialog: false,
+      dialog2: false,
+      cantidad:"",
       page: 1,
       pageCount: 0,
       itemsPerPage: 10,
@@ -65,13 +98,14 @@ export default {
       config: "",
       headers: [
         {
-          text: "id",
+          text: "Nombre",
           align: "left",
-          value: "id"
+          value: "name"
         },
         { text: "Stock", value: "quantity" },
         { text: "Precio($)", value: "price" },
-        { text: "Iva", value: "tax" }
+        { text: "Iva", value: "tax" },
+        { text: "Acciones", value: "action", sortable: false }
       ],
       desserts: []
     };
@@ -84,13 +118,17 @@ export default {
         Authorization: "token " + this.token
       }
     };
-    axios.get(this.url + "inventories/", this.config).then(response => {
-      this.desserts = response.data;
-    });
+    this.verInventario()
   },
   methods: {
+    verInventario(){
+      axios.get(this.url + "inventories/", this.config).then(response => {
+        this.desserts = response.data;
+      });
+    },
     close() {
       this.dialog = false;
+      this.dialog2= false;
     },
     save() {
       if (this.editedIndex > -1) {
@@ -99,6 +137,29 @@ export default {
         this.desserts.push(this.editedItem);
       }
       this.close();
+    },
+    vender(item) {
+      let params = {
+        "id": item.id,
+        "price": item.price,
+        "tax": item.tax,
+        "product": item.product,
+        "user": item.user,
+        "acction": 2,
+        "quantity": this.cantidad
+      };
+      console.log(params)
+      axios.put(this.url + "inventories/venta/"+item.id+"/", params, this.config)
+        .then(response => {
+          console.log(response.data)
+        });
+      this.verInventario();
+      this.dialog2= false;
+    },
+    deleteItem(item) {
+      axios.get(this.url + "inventories/", this.config).then(response => {
+        this.desserts = response.data;
+      });
     }
   }
 };
